5decdf18a63859f459153dfa12cc2f05ad3d18f5
okonomi <oknm@okonomi.sakura.ne.jp>
2017-06-04 08:14:26 +0900

    wip

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import MyEditor from './MyEditor';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<MyEditor />, document.getElementById('root'));
 registerServiceWorker();
