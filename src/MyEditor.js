8dd3224de79ed0d0ebbae374045d5889a7698bdc
okonomi <oknm@okonomi.sakura.ne.jp>
2017-06-04 08:53:38 +0900

    wip

---
+++ src/MyEditor.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {Editor, EditorState, RichUtils} from 'draft-js';
+import {stateToMarkdown} from 'draft-js-export-markdown';
+
+class MyEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {editorState: EditorState.createEmpty(), md: ''};
+    this.onChange = (editorState) => this.setState({editorState});
+    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+  }
+  handleKeyCommand(command) {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
+  }
+  _onBoldClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  }
+  render() {
+    return (
+      <div>
+        <button onMouseDown={this._onBoldClick.bind(this)}>Bold</button>
+        <button onMouseDown={(e) => {
+          this.onChange(
+            RichUtils.toggleBlockType(this.state.editorState, 'unordered-list-item')
+          );
+          e.preventDefault();
+        }}>List</button>
+        <button onMouseDown={(e) => {
+          this.setState({
+            md: stateToMarkdown(this.state.editorState.getCurrentContent())
+          });
+          e.preventDefault();
+        }}>Export</button>
+        <Editor
+          editorState={this.state.editorState}
+          handleKeyCommand={this.handleKeyCommand}
+          onChange={this.onChange}
+          onTab={(e) => {
+            console.log('tab')
+          }}
+        />
+        <pre>
+          {this.state.md}
+        </pre>
+      </div>
+    );
+  }
+}
+
+export default MyEditor;
