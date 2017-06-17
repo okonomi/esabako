12ea87cafa9206c326e2a511a757744beee80fc6
okonomi <oknm@okonomi.sakura.ne.jp>
2017-06-17 13:54:40 +0900

    fix layout

--- src/MyEditor.js
+++ src/MyEditor.js
@@ -33,78 +33,82 @@ class MyEditor extends React.Component {
   render() {
     return (
       <div className='MyEditor'>
-        <button onMouseDown={this._onBoldClick.bind(this)}>Bold</button>
-        <button onMouseDown={(e) => {
-          this.onChange(
-            RichUtils.toggleBlockType(this.state.editorState, 'unordered-list-item')
-          );
-          e.preventDefault();
-        }}>List</button>
-        <button onMouseDown={(e) => {
-          this.setState({
-            md: stateToMarkdown(this.state.editorState.getCurrentContent())
-          });
-          e.preventDefault();
-        }}>Export</button>
-        <button onMouseDown={(e) => {
-          var instance = axios.create({
-            baseURL: 'http://localhost:8080',
-            // timeout: 3000,
-            headers: {
-              'Authorization': 'Bearer a768efa2acb0757e4621b2902bf4364afa959d77185b7d8fbb1e46a8b66c8ef8',
-              'Target-URL': 'https://api.esa.io',
-            },
-          });
-          instance.get('/v1/teams/okonomi/posts/344')
-            .then((response) => {
-              console.log(response);
-
-              this.setState({post: response.data});
-
-              this.setState({title: this.state.post.name});
-
-              this.onChange(
-                EditorState.createWithContent(stateFromMarkdown(
-                  this.state.post.body_md
-                ))
-              )
-            })
-            .catch((error) => {
-              console.log(error);
+        <div>
+          <button onMouseDown={this._onBoldClick.bind(this)}>Bold</button>
+          <button onMouseDown={(e) => {
+            this.onChange(
+              RichUtils.toggleBlockType(this.state.editorState, 'unordered-list-item')
+            );
+            e.preventDefault();
+          }}>List</button>
+          <button onMouseDown={(e) => {
+            this.setState({
+              md: stateToMarkdown(this.state.editorState.getCurrentContent())
             });
-        }}>Load</button>
-        <button onMouseDown={(e) => {
-          var post = this.state.post;
-          post.body_md = stateToMarkdown(this.state.editorState.getCurrentContent());
-          post.name = this.state.title;
-
-          this.setState({post: post});
-
-          var instance = axios.create({
-            baseURL: 'http://localhost:8080',
-            timeout: 1000,
-            headers: {
-              'Authorization': 'Bearer a768efa2acb0757e4621b2902bf4364afa959d77185b7d8fbb1e46a8b66c8ef8',
-              'Target-URL': 'https://api.esa.io',
-            }
-          });
-          instance.patch('/v1/teams/okonomi/posts/344', {
-            post: post
-          })
-            .then((response) => {
-              console.log(response);
-
-              // this.props.onLoad(response);
-            })
-            .catch((error) => {
-              console.log(error);
+            e.preventDefault();
+          }}>Export</button>
+          <button onMouseDown={(e) => {
+            var instance = axios.create({
+              baseURL: 'http://localhost:8080',
+              // timeout: 3000,
+              headers: {
+                'Authorization': 'Bearer a768efa2acb0757e4621b2902bf4364afa959d77185b7d8fbb1e46a8b66c8ef8',
+                'Target-URL': 'https://api.esa.io',
+              },
             });
+            instance.get('/v1/teams/okonomi/posts/344')
+              .then((response) => {
+                console.log(response);
 
-          e.preventDefault();
-        }}>Save</button>
-        <input type="text" value={this.state.title} onChange={(e) => {
-          this.setState({title: e.target.value});
-        }} />
+                this.setState({post: response.data});
+
+                this.setState({title: this.state.post.name});
+
+                this.onChange(
+                  EditorState.createWithContent(stateFromMarkdown(
+                    this.state.post.body_md
+                  ))
+                )
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+          }}>Load</button>
+          <button onMouseDown={(e) => {
+            var post = this.state.post;
+            post.body_md = stateToMarkdown(this.state.editorState.getCurrentContent());
+            post.name = this.state.title;
+
+            this.setState({post: post});
+
+            var instance = axios.create({
+              baseURL: 'http://localhost:8080',
+              timeout: 1000,
+              headers: {
+                'Authorization': 'Bearer a768efa2acb0757e4621b2902bf4364afa959d77185b7d8fbb1e46a8b66c8ef8',
+                'Target-URL': 'https://api.esa.io',
+              }
+            });
+            instance.patch('/v1/teams/okonomi/posts/344', {
+              post: post
+            })
+              .then((response) => {
+                console.log(response);
+
+                // this.props.onLoad(response);
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+
+            e.preventDefault();
+          }}>Save</button>
+        </div>
+        <div>
+          <input type="text" value={this.state.title} onChange={(e) => {
+            this.setState({title: e.target.value});
+          }} />
+        </div>
         <Editor
           editorState={this.state.editorState}
           handleKeyCommand={this.handleKeyCommand}
