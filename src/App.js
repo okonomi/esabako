23d72fbd5977ac422e59f37332c5734f77bce806
okonomi <oknm@okonomi.sakura.ne.jp>
2017-06-25 19:48:50 +0900

    change app name and description

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>esabako</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          esabako is esa.io client
         </p>
 
         <MyEditor />
