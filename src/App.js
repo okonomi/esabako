bb34ae009e10f2614c200bebf79a658291039872
okonomi <oknm@okonomi.sakura.ne.jp>
2017-07-01 18:29:09 +0900

    change logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import MyEditor from './MyEditor';
 
@@ -8,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <span className="App-logo" role="img" aria-labelledby="logo">🐔</span>
           <h2>esabako</h2>
         </div>
         <p className="App-intro">
