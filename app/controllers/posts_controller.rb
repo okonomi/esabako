3b1b45dabc228ccc9599a7d097ab0845bc10ddc0
okonomi <oknm@okonomi.sakura.ne.jp>
2017-12-31 15:53:42 +0900

    Show post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -64,7 +64,9 @@ class PostsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post = Post.find(params[:id])
+      client = Esa::Client.new(current_team: 'okonomi')
+      @post = Post.new(client.post(params[:id]).body.select { |key, _| %w[number name body_md body_html].include? key })
+      @post.id = @post.number
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post
+  include ActiveModel::Model
 
+  attr_accessor :id,
+                :number,
+                :name,
+                :body_md,
+                :body_html
 end
