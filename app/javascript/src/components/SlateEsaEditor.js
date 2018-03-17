5aa4bb0259e1b2e33e49a43f13f06360c5ee2be0
okonomi <oknm@okonomi.sakura.ne.jp>
2018-03-18 00:56:28 +0900

    Show post

--- app/javascript/src/components/SlateEsaEditor.js
+++ app/javascript/src/components/SlateEsaEditor.js
@@ -1,7 +1,8 @@
+import React from 'react'
 import { Editor } from 'slate-react'
 import { Value } from 'slate'
-
-import React from 'react'
+import Html from 'slate-html-serializer'
+import EditorUtils from './EditorUtils'
 
 const initialValue = {
   "document": {
@@ -83,6 +84,119 @@ const initialValue = {
   }
 }
 
+/**
+ * Tags to blocks.
+ *
+ * @type {Object}
+ */
+
+const BLOCK_TAGS = {
+  p: 'paragraph',
+  li: 'list-item',
+  ul: 'bulleted-list',
+  ol: 'numbered-list',
+  blockquote: 'quote',
+  pre: 'code',
+  h1: 'heading-one',
+  h2: 'heading-two',
+  h3: 'heading-three',
+  h4: 'heading-four',
+  h5: 'heading-five',
+  h6: 'heading-six',
+}
+
+/**
+ * Tags to marks.
+ *
+ * @type {Object}
+ */
+
+const MARK_TAGS = {
+  strong: 'bold',
+  em: 'italic',
+  u: 'underline',
+  s: 'strikethrough',
+  code: 'code',
+}
+
+/**
+ * Serializer rules.
+ *
+ * @type {Array}
+ */
+
+const RULES = [
+  {
+    deserialize(el, next) {
+      const block = BLOCK_TAGS[el.tagName.toLowerCase()]
+      if (!block) return
+      return {
+        object: 'block',
+        type: block,
+        nodes: next(el.childNodes),
+      }
+    },
+  },
+  {
+    deserialize(el, next) {
+      const mark = MARK_TAGS[el.tagName.toLowerCase()]
+      if (!mark) return
+      return {
+        object: 'mark',
+        type: mark,
+        nodes: next(el.childNodes),
+      }
+    },
+  },
+  {
+    // Special case for code blocks, which need to grab the nested childNodes.
+    deserialize(el, next) {
+      if (el.tagName.toLowerCase() != 'pre') return
+      const code = el.childNodes[0]
+      const childNodes =
+        code && code.tagName.toLowerCase() == 'code'
+          ? code.childNodes
+          : el.childNodes
+
+      return {
+        object: 'block',
+        type: 'code',
+        nodes: next(childNodes),
+      }
+    },
+  },
+  {
+    // Special case for images, to grab their src.
+    deserialize(el, next) {
+      if (el.tagName.toLowerCase() != 'img') return
+      return {
+        object: 'block',
+        type: 'image',
+        isVoid: true,
+        nodes: next(el.childNodes),
+        data: {
+          src: el.getAttribute('src'),
+        },
+      }
+    },
+  },
+  {
+    // Special case for links, to grab their href.
+    deserialize(el, next) {
+      if (el.tagName.toLowerCase() != 'a') return
+      return {
+        object: 'inline',
+        type: 'link',
+        nodes: next(el.childNodes),
+        data: {
+          href: el.getAttribute('href'),
+        },
+      }
+    },
+  },
+]
+
+const serializer = new Html({ rules: RULES })
 
 /**
  * The auto-markdown example.
@@ -106,7 +220,8 @@ class SlateEsaEditor extends React.Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    
+    const html = EditorUtils.convertMarkdownToHtml(nextProps.post.body)
+    this.setState({ value: serializer.deserialize(html) })
   }
 
   /**
@@ -154,7 +269,7 @@ class SlateEsaEditor extends React.Component {
 
   render() {
     return (
-      <div className="editor">
+      <div className="markdown-body">
         <Editor
           placeholder="Write some markdown..."
           value={this.state.value}
