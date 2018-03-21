a2d60bd7061ec79efe71aa0fbb34a8808938d47e
okonomi <oknm@okonomi.sakura.ne.jp>
2018-03-21 15:50:10 +0900

    Add test (it is fail)

--- app/javascript/src/components/Editor/Serializer.test.js
+++ app/javascript/src/components/Editor/Serializer.test.js
@@ -1,1 +1,38 @@
 import Serializer from './Serializer'
+import Plain from 'slate-plain-serializer'
+import { Value } from 'slate'
+
+describe('#deserialize', () => {
+  test('line breaks handling', () => {
+    const value = Value.fromJSON({
+      "object": "value",
+      "document": {
+        "object": "document",
+        "data": {},
+        "nodes": [
+          {
+            "object": "block",
+            "type": "paragraph",
+            "isVoid": false,
+            "data": {},
+            "nodes": [
+              {
+                "object": "text",
+                "leaves": [
+                  {
+                    "object": "leaf",
+                    "text": "あいうえお\nかきくけこ",
+                    "marks": []
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    })
+
+    const serializer = new Serializer()
+    expect(serializer.serialize(value)).toEqual("あいうえお\nかきくけこ")
+  })
+})
