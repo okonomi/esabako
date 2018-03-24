6ead8f94742095ab9379ff553b75dc2b9b18cf1b
okonomi <oknm@okonomi.sakura.ne.jp>
2018-03-24 18:54:37 +0900

    Remove imcomplete test

--- app/javascript/src/components/Editor/EditorUtils.test.js
+++
@@ -1,8 +0,0 @@
-import EditorUtils from './EditorUtils'
-
-describe('#convertHtmlToMarkdown', () => {
-  test('<br /> convertion', () => {
-
-    expect(EditorUtils.convertHtmlToMarkdown('<p>あいうえお<br />かきくけこ</p>')).toEqual("あいうえお\nかきくけこ")
-  })
-})
