1fe3d6e5008e42630c4b5f49d19563c7b14ef0d3
okonomi <oknm@okonomi.sakura.ne.jp>
2018-04-04 08:22:16 +0900

    Add callback routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resource :sandbox, only: :show
   resources :posts
+
+  get '/auth/:provider/callback' => 'sessions#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
