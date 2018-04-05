5f77845cfa02180b5ebeee9577324c520157c368
okonomi <oknm@okonomi.sakura.ne.jp>
2018-04-05 22:03:12 +0900

    Imprement handling access token

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_auth_hash(request.env['omniauth.auth'])
     session[:user_id] = user.id
+    session[:token] = request.env['omniauth.auth'][:credentials][:token]
     redirect_to '/', notice: 'ログインしました'
   end
 end
