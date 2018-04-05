0827002717426b53c0d9fc05f417f64e41a82286
okonomi <oknm@okonomi.sakura.ne.jp>
2018-04-06 08:18:28 +0900

    Save token to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, omniauth_providers: %i[esa]
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.nickname = auth.info.nickname
-      user.image_url = auth.info.image_url
+      user.image_url = auth.info.image
+      user.token = auth.credentials.token
     end
   end
 end
