7dfbc26216fcc5e54261de8b9f08a2c335f27721
okonomi <oknm@okonomi.sakura.ne.jp>
2018-04-09 22:52:24 +0900

    wip

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -66,8 +66,7 @@ class PostsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      client = Esa::Client.new(access_token: current_user.token, current_team: session['team'])
-      @post = Post.new(client.post(params[:id]).body.select { |key, _| %w[number name body_md body_html].include? key })
+      @post = Post.find(params[:id], _team_name: 'okonomi')
       @post.id = @post.number
     end
 
