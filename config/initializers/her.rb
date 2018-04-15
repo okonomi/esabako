d67ba393b574b2f0353d83c3fa1fba8e56f38ad7
okonomi <oknm@okonomi.sakura.ne.jp>
2018-04-15 16:33:18 +0900

    Use user's token for her oauth

--- config/initializers/her.rb
+++ config/initializers/her.rb
@@ -1,14 +1,14 @@
-Her::API.setup url: 'https://api.esa.io/v1' do |c|
-  # Request
-  c.use FaradayMiddleware::OAuth2, ENV['ESA_ACCESS_TOKEN'], token_type: :bearer
-  c.use Faraday::Request::UrlEncoded
+# Her::API.setup url: 'https://api.esa.io/v1' do |c|
+#   # Request
+#   c.use FaradayMiddleware::OAuth2, ENV['ESA_ACCESS_TOKEN'], token_type: :bearer
+#   c.use Faraday::Request::UrlEncoded
 
-  # Response
-  c.use Her::Middleware::DefaultParseJSON
+#   # Response
+#   c.use Her::Middleware::DefaultParseJSON
 
-  c.use FaradayMiddleware::Instrumentation
-  c.use Faraday::Response::Logger
+#   c.use FaradayMiddleware::Instrumentation
+#   c.use Faraday::Response::Logger
 
-  # Adapter
-  c.use Faraday::Adapter::NetHttp
-end
+#   # Adapter
+#   c.use Faraday::Adapter::NetHttp
+# end
