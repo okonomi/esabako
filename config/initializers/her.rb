72c445b539c1b01258a8e22431cd3b36b4f0178b
okonomi <oknm@okonomi.sakura.ne.jp>
2018-04-15 15:42:57 +0900

    WIP: Move token to env

--- config/initializers/her.rb
+++ config/initializers/her.rb
@@ -1,6 +1,6 @@
 Her::API.setup url: 'https://api.esa.io/v1' do |c|
   # Request
-  c.use FaradayMiddleware::OAuth2, '4ac04e54ba0726ccb3135231a76ec6ca0e83586533b00ae1af53f371404ffed9', token_type: :bearer
+  c.use FaradayMiddleware::OAuth2, ENV['ESA_ACCESS_TOKEN'], token_type: :bearer
   c.use Faraday::Request::UrlEncoded
 
   # Response
