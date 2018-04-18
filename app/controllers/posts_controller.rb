3baf2afb9b4600be059900e0acb486ec293fd454
okonomi <oknm@okonomi.sakura.ne.jp>
2018-04-19 08:36:42 +0900

    Fix template not found

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,7 +31,7 @@ class PostsController < ApplicationController
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
+        format.json { render json: { message: 'ok' }, status: :ok }
       else
         format.html { render :new }
         format.json { render json: @post.errors, status: :unprocessable_entity }
