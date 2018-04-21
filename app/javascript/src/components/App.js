2646ba55416f4d9df02f77a4a4a7187556f7c07e
okonomi <oknm@okonomi.sakura.ne.jp>
2018-04-21 17:18:21 +0900

    Switch theme to bootstrap

--- app/javascript/src/components/App.js
+++ app/javascript/src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import NotificationsSystem from 'reapop'
-import theme from 'reapop-theme-wybo'
+import theme from 'reapop-theme-bootstrap'
 import Editor from './../containers/Editor'
 
 const App = (props) => {
