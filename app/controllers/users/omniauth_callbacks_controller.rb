bf4139d0d69bec788242ce83ce3c2b645bd254d3
okonomi <oknm@okonomi.sakura.ne.jp>
2018-04-25 08:26:18 +0900

    Remove team find when omniauth callback

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,10 +4,6 @@ module Users
       @user = User.from_omniauth(request.env['omniauth.auth'])
 
       if @user.persisted?
-        # default: first team
-        team = Team.all.first
-        session['team'] = team.name
-  
         sign_in_and_redirect @user, event: :authentication
         set_flash_message(:notice, :success, kind: 'esa') if is_navigational_format?
       else
