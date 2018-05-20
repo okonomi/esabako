67fa61a233264bf35378e45735cc049175238c15
okonomi <oknm@okonomi.sakura.ne.jp>
2018-05-21 08:43:09 +0900

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,10 @@ module.exports = {
     "eslint:recommended",
     "plugin:react/recommended"
   ],
+  "env": {
+    "browser": true
+  },
+  "parser": "babel-eslint",
   "parserOptions": {
     "ecmaVersion": 6,
     "sourceType": "module"
