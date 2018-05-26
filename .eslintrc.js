c6dafeeacf29c7358fd000acad8df44d7e76bc90
okonomi <oknm@okonomi.sakura.ne.jp>
2018-05-26 15:48:36 +0900

    Solve jest errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   "extends": [
     "eslint:recommended",
-    "plugin:react/recommended"
+    "plugin:react/recommended",
+    "plugin:jest/recommended"
   ],
   "env": {
     "browser": true
