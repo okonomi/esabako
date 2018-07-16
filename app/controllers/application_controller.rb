56bc7d39b9de858a9f967308a5ed3a9910fc594d
okonomi <oknm@okonomi.sakura.ne.jp>
2018-07-16 19:45:02 +0900

    Fix rubocop Lint/UnusedMethodArgument

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   before_action :initialize_her
 
-  def new_session_path(scope)
+  def new_session_path(_scope)
     new_user_session_path
   end
 
